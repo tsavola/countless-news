0bfcd0f9b0e27a8efae8d0cc789454ba83b19d13
Timo Savola <timo.savola@iki.fi>
2017-11-03 20:39:32 +0200

    responsive web design

--- render.go
+++ render.go
@@ -26,50 +26,29 @@ import (
 )
 
 const (
-	scoreWidth = 20 // pt
+	scoreColor    = "#f80"
+	maxScoreWidth = 50 // pt
 )
 
 func Render(index Index, maxScore float64) []byte {
 	buf := new(bytes.Buffer)
 
-	fmt.Fprint(buf, `<!DOCTYPE html>`)
-	fmt.Fprint(buf, `<html lang="en">`)
-	fmt.Fprint(buf, `<head>`)
-	fmt.Fprint(buf, `<meta charset="utf-8">`)
-	fmt.Fprint(buf, `<title>Countless News</title>`)
-	fmt.Fprint(buf, `<meta name="description" content="Top news by country">`)
-	fmt.Fprint(buf, `<style>`)
-	fmt.Fprintln(buf, `a { text-decoration: none }`)
-	fmt.Fprintln(buf, `table { margin-left: auto; margin-right: auto }`)
-	fmt.Fprintln(buf, `tr:nth-child(odd) { background-color: white }`)
-	fmt.Fprintln(buf, `tr:nth-child(even) { background-color: #f9f9f9 }`)
-	fmt.Fprintln(buf, `td { white-space: nowrap }`)
-	fmt.Fprintln(buf, `.header { display: none }`)
-	fmt.Fprintln(buf, `.nation { padding-left: 4pt; padding-right: 4pt }`)
-	fmt.Fprintln(buf, `.score { vertical-align: bottom; padding-right: 2pt }`)
-	fmt.Fprintf(buf, ".score div { width: %dpt; height: 1em }\n", scoreWidth)
-	fmt.Fprintln(buf, `.score div div { height: 100%; background-color: lightgrey; margin-left: auto }`)
-	fmt.Fprintln(buf, `.domain { opacity: 0.5; text-align: right; color: #444; padding-left: 4pt }`)
-	fmt.Fprintln(buf, `.https { color: green }`)
-	fmt.Fprintln(buf, `#footer { background-color: inherit }`)
-	fmt.Fprintln(buf, `#footer td { text-align: right; padding-top: 1em; opacity: 0.5 }`)
-	fmt.Fprint(buf, `</style>`)
-	fmt.Fprint(buf, `</head>`)
-	fmt.Fprint(buf, `<body>`)
-	fmt.Fprint(buf, `<table>`)
-	fmt.Fprint(buf, `<tr class="header">`)
-	fmt.Fprint(buf, `<th>Flag</th>`)
-	fmt.Fprint(buf, `<th>Nation</th>`)
-	fmt.Fprint(buf, `<th>Score</th>`)
-	fmt.Fprint(buf, `<th>Headline</th>`)
-	fmt.Fprint(buf, `<th>Source</th>`)
-	fmt.Fprint(buf, `</tr>`)
+	fmt.Fprintf(buf, `<!DOCTYPE html>`)
+	fmt.Fprintf(buf, `<html lang="en">`)
+	fmt.Fprintf(buf, `<head>`)
+	fmt.Fprintf(buf, `<meta charset="utf-8">`)
+	fmt.Fprintf(buf, `<title>Countless News</title>`)
+	fmt.Fprintf(buf, `<meta name="description" content="Top news by country">`)
+	fmt.Fprintf(buf, `<meta name="viewport" content="width=device-width, initial-scale=1.0">`)
+	fmt.Fprintf(buf, `<link rel="stylesheet" type="text/css" href="style.css">`)
+	fmt.Fprintf(buf, `</head>`)
+	fmt.Fprintf(buf, `<body>`)
+	fmt.Fprintf(buf, `<h1>Countless News</h1>`)
+	fmt.Fprintf(buf, `<div id="root">`)
+	fmt.Fprintf(buf, `<div id="news">`)
 
 	for _, entry := range index {
-		headline := entry.Headline
-		if len(headline) > 100 {
-			headline = headline[:99] + "…"
-		}
+		headline := html.UnescapeString(entry.Headline)
 
 		domain := entry.URL.Host
 
@@ -79,33 +58,31 @@ func Render(index Index, maxScore float64) []byte {
 		parts := strings.Split(domain, ".")
 		domain = strings.Join(parts[len(parts)-num:len(parts)], ".")
 
-		// TODO: something better
-		var proxyHTML string
-		if domain == "google.com" {
-			entry.URL.Scheme = "https"
-			proxyHTML = fmt.Sprintf(`<span class="%s">%s</span>, `, entry.URL.Scheme, domain)
-			domain = "baltictimes.com"
-		}
+		scoreWidth := entry.Score * maxScoreWidth / maxScore
 
-		fmt.Fprint(buf, `<tr>`)
-		fmt.Fprintf(buf, `<td>%s</td>`, entry.Nation.Flag)
-		fmt.Fprintf(buf, `<td class="nation">%s</td>`, html.EscapeString(entry.Nation.Name))
-		fmt.Fprintf(buf, `<td class="score"><div title="%f"><div style="width: %.2fpt"></div></div></td>`, entry.Score, entry.Score*scoreWidth/maxScore)
-		fmt.Fprintf(buf, `<td class="headline"><a href="%s" title="%s">%s</a></td>`, entry.URL, html.EscapeString(entry.Timestamp), html.EscapeString(headline))
-		fmt.Fprintf(buf, `<td class="domain">%s<span class="%s">%s</span></td>`, proxyHTML, entry.URL.Scheme, domain)
-		fmt.Fprint(buf, `</tr>`)
+		fmt.Fprintf(buf, `<div class="item">`)
+		fmt.Fprintf(buf, `<div>`)
+		fmt.Fprintf(buf, `<div class="nation" style="background: linear-gradient(-90deg, %s, transparent %.3fpt)">`, scoreColor, scoreWidth)
+		fmt.Fprintf(buf, `<span class="flag">%s</span>`, entry.Nation.Flag)
+		fmt.Fprintf(buf, `<span class="name">%s</span>`, html.EscapeString(entry.Nation.Name))
+		fmt.Fprintf(buf, `</div>`) // nation
+		fmt.Fprintf(buf, `<div class="headline"><a href="%s" title="%s">%s</a></div>`, entry.URL, html.EscapeString(entry.Timestamp), html.EscapeString(headline))
+		fmt.Fprintf(buf, `<div class="domain"><span class="%s">%s</span></div>`, entry.URL.Scheme, domain)
+		fmt.Fprintf(buf, `</div>`) // anonymous
+		fmt.Fprintf(buf, `</div>`) // item
 	}
 
-	fmt.Fprint(buf, `<tr id="footer">`)
-	fmt.Fprint(buf, `<td colspan="5">`)
-	fmt.Fprint(buf, `<a href="https://github.com/tsavola/countless-news/blob/master/algorithm.go">Algorithm</a>`)
-	fmt.Fprint(buf, ` · `)
-	fmt.Fprint(buf, `<a href="https://github.com/tsavola/countless-news/blob/master/sources.go">Sources</a>`)
-	fmt.Fprint(buf, `</td>`)
-	fmt.Fprint(buf, `</tr>`)
-	fmt.Fprint(buf, `</table>`)
-	fmt.Fprint(buf, `</body>`)
-	fmt.Fprint(buf, `</html>`)
+	fmt.Fprintf(buf, `</div>`) // news
+	fmt.Fprintf(buf, `<div id="footer">`)
+	fmt.Fprintf(buf, `<div>Local news for nonlocal people</div>`)
+	fmt.Fprintf(buf, `<div>`)
+	fmt.Fprintf(buf, `<div><a href="https://github.com/tsavola/countless-news/blob/master/algorithm.go">Algorithm</a></div>`)
+	fmt.Fprintf(buf, `<div><a href="https://github.com/tsavola/countless-news/blob/master/sources.go">Sources</a></div>`)
+	fmt.Fprintf(buf, `</div>`) // anonymous
+	fmt.Fprintf(buf, `</div>`) // footer
+	fmt.Fprintf(buf, `</div>`) // root
+	fmt.Fprintf(buf, `</body>`)
+	fmt.Fprintf(buf, `</html>`)
 
 	return buf.Bytes()
 }
